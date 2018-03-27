77755dff5b3a56d12e7e295445ae4fb6a3145743
Bartłomiej Bajda <allerbbb@gmail.com>
2018-03-27 23:42:37 +0000

    add program.js

---
+++ program.js
@@ -0,0 +1,23 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function () {
+    process.stdout.write("choose between: lang, version, /exit \n")
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app! \n');
+                process.exit();
+                break;
+            case 'lang':
+                process.stdout.write(process.env.LANG + '\n');
+                break;
+            case 'version':
+                process.stdout.write(process.versions.node + '\n');
+                break;
+            default:
+                process.stderr.write('Wrong instruction! \n');
+                break;
+        }
+    }
+});
